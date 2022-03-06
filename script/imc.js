5ac24fb7722fbee9fdd1860571fb90bc1f16d705
Carlos Matheus <97807823+CarlosMatheusPS@users.noreply.github.com>
2022-03-06 04:29:56 -0300

    Initial commit

---
+++ script/imc.js
@@ -0,0 +1,30 @@
+const calcular = document.getElementById("calcular");
+
+function imc() {
+
+    const nome = document.getElementById("nome").value;
+    const altura = document.getElementById("altura").value;
+    const peso = document.getElementById("peso").value;
+    const resultado = document.getElementById("resultado");
+
+    if (nome === '' || altura === '' || peso === '') {
+        resultado.textContent = `[ERRO] Preenche todos os dados!`
+    } else {
+        const imc = (peso / (altura * altura));
+        let classsificacao = '';
+        if (imc < 18.5) {
+            classsificacao = 'abaixo do peso.'
+        } else if (imc < 25) {
+            classsificacao = 'no peso normal.'
+        } else if (imc < 30) {
+            classsificacao = 'com sobrepeso.'
+        } else if (imc < 35) {
+            classsificacao = 'com Obesidade Grau I.'
+        } else if (imc < 40) {
+            classsificacao = 'com Obresidade Grau II.'
+        } else {
+            classsificacao = 'com Obesidade Grau III.'
+        }
+        resultado.textContent = `${nome}, seu imc é: ${Math.round(imc)} e você está ${classsificacao}`
+    }
+}
